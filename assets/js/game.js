a3ba7147b3ead7c24b63588308dabbeb6b18fe58
dangkmemez <98855240+dangkmemez@users.noreply.github.com>
2022-03-09 21:29:06 -0500

    added check health status

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,10 +18,18 @@ var fight = function() {
     // Log a resulting message to the console so we know that it worked.
   console.log (
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
-  )
+  );
+  // check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-  
+    playerHealth = playerHealth - enemyAttack;
     // Log a resulting message to the console so we know that it worked.
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   };
   
 
